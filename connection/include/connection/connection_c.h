19fc66abe0ede32eaac10bb200ff47f667520443
Juan Vázquez Moreno <juan.vazquez-moreno_ext@michelin.com>
2022-01-13 20:42:38 +0100

    minor fix

--- connection/include/connection/connection_c.h
+++ connection/include/connection/connection_c.h
@@ -11,10 +11,10 @@ extern "C" {
 
 __attribute__ ((warn_unused_result)) struct connection_result connection_execute_many(Connection* const conn, const char* const stmt, void*);
 __attribute__ ((warn_unused_result)) struct connection_result connection_execute(Connection* const conn, const char* const stmt);
-__attribute__ ((warn_unused_result)) const char* connection_close(Connection* const conn);
-__attribute__ ((warn_unused_result)) const char* connection_begin(Connection* const conn);
-__attribute__ ((warn_unused_result)) const char* connection_commit(Connection* const conn);
-__attribute__ ((warn_unused_result)) const char* connection_rollback(Connection* const conn);
+__attribute__ ((warn_unused_result)) conn_state connection_close(Connection* const conn);
+__attribute__ ((warn_unused_result)) conn_state connection_begin(Connection* const conn);
+__attribute__ ((warn_unused_result)) conn_state connection_commit(Connection* const conn);
+__attribute__ ((warn_unused_result)) conn_state connection_rollback(Connection* const conn);
 __attribute__ ((warn_unused_result)) const char* connection_error_message(Connection* const conn);
 __attribute__ ((warn_unused_result)) unsigned connection_changes(Connection* const conn);
 void free_connection(Connection* const conn);
